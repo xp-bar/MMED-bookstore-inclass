7594e2e0ce0c5375cea0df7c056892c696c7a46c
Nicholas Ireland <nick@nicholasireland.ca>
2026-03-04 20:36:00 -0500

    Add: Authors model, migration, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // BookSeeder::class === Database\Seeders\BookSeeder
         $this->call([
-            BookSeeder::class
+            AuthorSeeder::class,
+            BookSeeder::class,
         ]);
     }
 }
