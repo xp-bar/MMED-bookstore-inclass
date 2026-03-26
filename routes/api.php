31c59d1892eaa794d770664f0ed697db6cb3cdfa
Nicholas Ireland <nick@nicholasireland.ca>
2026-03-25 21:51:08 -0400

    Add: authentication
    Note: as of end of class, the authentication is not working correctly; I will attempt to get this working as soon as I can.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/login', [AuthController::class, 'login'])->middleware('web');
+Route::get('/me', [AuthController::class, 'me'])
+    // ->middleware('auth:sanctum')
+    ->middleware([
+        'auth:sanctum'
+    ]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
